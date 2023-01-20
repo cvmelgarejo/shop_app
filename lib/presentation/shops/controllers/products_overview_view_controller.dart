353568f4c5c098d29808ef53ce241c0c9ccebf3e
cvmelgarejo <cvmelgarejo@hotmail.com>
2023-01-20 18:21:23 -0300

    add products overview view controller and clean view

---
+++ lib/presentation/shops/controllers/products_overview_view_controller.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/foundation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shop_app/core/dependency_injection/locator.dart';
+import 'package:shop_app/domain/models/product.dart';
+import 'package:shop_app/domain/repositories/product_repository.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class ProducstOverviewViewController extends ChangeNotifier {
+  final ProductRepository _productRepository;
+  final NavigationService _navigationService;
+
+  List<Product> _products = [];
+  List<Product> get products => _products;
+
+  ProducstOverviewViewController({
+    required ProductRepository productRepository,
+    required NavigationService navigationService,
+  })  : _productRepository = productRepository,
+        _navigationService = navigationService;
+
+  Future<void> getProducts() async {
+    _products = await _productRepository.getProducts();
+    notifyListeners();
+  }
+
+  void goToProductDetails({required Product? product}) {
+    _navigationService.navigateTo('/product_details', arguments: {
+      'product': product,
+    });
+  }
+
+  Future<void> fetchStates() async {
+    _products = [];
+
+    final promises = [
+      getProducts(),
+    ];
+
+    await Future.wait(promises);
+  }
+}
+
+final productsOverviewProvider = ChangeNotifierProvider(
+  (ref) => ProducstOverviewViewController(
+    productRepository: locator<ProductRepository>(),
+    navigationService: locator<NavigationService>(),
+  ),
+);
--- lib/presentation/shops/views/products_overview_view.dart
+++ lib/presentation/shops/views/products_overview_view.dart
@@ -1,45 +1,35 @@
 import 'package:flutter/material.dart';
-import '/presentation/shops/widgets/product_item_widget.dart';
-import '/domain/models/product.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/src/hooks.dart';
+import 'package:shop_app/presentation/shops/widgets/product_item_widget.dart';
+import 'package:shop_app/presentation/shops/controllers/products_overview_view_controller.dart';
 
-class ProductsOverviewView extends StatelessWidget {
-  final List<Product> loadedProducts = [
-    Product(
-      id: 'p1',
-      name: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product(
-      id: 'p2',
-      name: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 59.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/11/29/05/45/adult-1867284_1280.jpg',
-    ),
-    Product(
-      id: 'p3',
-      name: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 19.99,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    Product(
-      id: 'p4',
-      name: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 49.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/11/18/17/46/pan-1835926_1280.jpg',
-    ),
-  ];
+class ProductsOverviewView extends StatefulHookConsumerWidget {
+  const ProductsOverviewView({Key? key}) : super(key: key);
 
   @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _ProductsOverviewViewState();
+}
+
+class _ProductsOverviewViewState extends ConsumerState<ProductsOverviewView> {
+  @override
   Widget build(BuildContext context) {
+    final productsOverviewViewController = ref.read(productsOverviewProvider);
+    final loadedProducts =
+        ref.watch(productsOverviewProvider.select((value) => value.products));
+
+    bool isLoading = false;
+    useEffect(() {
+      isLoading = true;
+
+      productsOverviewViewController.getProducts().then((value) {
+        setState(() {
+          isLoading = false;
+        });
+      });
+      return () {};
+    }, []);
     return Scaffold(
       appBar: AppBar(
         title: Text('My Shop'),
@@ -53,8 +43,13 @@ class ProductsOverviewView extends StatelessWidget {
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
         ),
-        itemBuilder: (ctx, i) => ProductItemWidget(loadedProducts[i].id,
-            loadedProducts[i].name, loadedProducts[i].imageUrl),
+        itemBuilder: (ctx, i) => ProductItemWidget(
+          loadedProducts[i],
+          ((product) => {
+                productsOverviewViewController.goToProductDetails(
+                    product: product)
+              }),
+        ),
       ),
     );
   }
